daab6ab746a4324cb403eb1e01d59d3271eb5b67
youssef <youssef@DELL>
2024-08-09 11:22:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using static ListGeneratorNameSpace.ListGenerator;
+namespace Assignment_LINQ_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
